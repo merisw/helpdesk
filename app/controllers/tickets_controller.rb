fdab48b800b64edadcfd4b807d215539f3f940f0
Meris Williams <merisw@gmail.com>
2013-06-06 13:44:11 -0700

    subject

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -37,7 +37,7 @@ class TicketsController < ApplicationController
   end
 
   def receive
-    ticket = Ticket.find_by_name(params[:headers][:Subject])
+    ticket = Ticket.find_by_name(params[:subject])
     ticket.response = params[:plain]
     ticket.save
 
